6a9b939ef359ce20e8453d0826e00a8d47c5d0b7
Nipendra Soni <rsoni2843@gmail.com>
2022-12-30 23:08:21 +0530

    data changes

--- src/data.js
+++ src/data.js
@@ -113,45 +113,46 @@ export const brands = [
 // projects
 export const projectsData = [
   {
+    id: "3",
+    image:
+      "https://user-images.githubusercontent.com/105920688/208405066-668ca6c8-6d31-4f17-a055-623228bed1a0.png",
+    name: "Care&Fit (Collaborative)",
+    github: "https://github.com/dinesh0085/trite-arithmetic-7774",
+    stack:
+      "React JS, Redux, TypeScript, Node JS, Express JS, Mongo DB, Chakra UI",
+    category: "Fitness",
+    description:
+      "Care&Fit is a health and fitness platform where you can buy online checkup tests and also book session for mind therapy and consult doctors.",
+    link: "https://moonlit-gumption-211089.netlify.app/",
+  },
+  {
     id: "2",
     image: Netmeds,
     name: "Netmeds (Individual)",
     github: "https://github.com/rsoni2843/brief-battle-8574",
-    stack:
-      "HTML, CSS, JavaScript, React JS, Redux, Chakra UI, Rest API, NPM, React Router DOM, Firebase, Mongo DB, Express JS.",
+    stack: "React JS, Redux, Firebase, Mongo DB, Express JS, Chakra UI",
     category: "e-commerce (medicine)",
     description:
       "Netmeds Marketplace Limited operates as an online pharmacy. TheCompany offers eyewear, fitness, personal care, prescribed medicines, and health products.",
-    link: "https://netmeds-rsoni2843.web.app/",
+    link: "https://charming-flan-ec1097.netlify.app/",
   },
   {
     id: 10,
     image: TravelGo,
     name: "TravelGo (Collaborative)",
     github: "https://github.com/rsoni2843/Tripadvisor-Clone",
-    stack:
-      "HTML, CSS, JavaScript, React JS, Redux, React-Redux, Chakra UI, NPM, React Router DOM, Node JS, Express JS, Mongo DB, Heroku, Firebase.",
+    stack: "React JS, Redux, Node JS, Express JS, Mongo DB, TypeScript",
     category: "travelling (Hotel Booking)",
     description:
       "TravelGo is a clone of Tripadvisor & Tripadvisor is an American online travel company that operates a website and mobile app with user-generated content and a comparison shopping website",
-    link: "https://tripadvisor-rsoni2843.web.app/",
+    link: "https://cheery-dasik-4fae30.netlify.app/",
   },
-  {
-    id: "3",
-    image: Orbitz,
-    name: "Orbitz (Collaborative)",
-    github: "https://github.com/rsoni2843/Orbitz.com-clone-",
-    stack: "HTML, CSS, JavaScript, Rest API.",
-    category: "Hotel Booking",
-    description:
-      "Orbitz.com is a travel website that enables consumers to search for and book a broad range of hotels, flights, rentals, cruises, vacation packages and destination activities.",
-    link: "https://cheery-kringle-54f279.netlify.app/",
-  },
+
   {
     id: "4",
     image: Mailtrap,
     name: "Mailtrap (Collaborative)",
-    stack: "HTML, CSS, JavaScript, JSON-Server.",
+    stack: "HTML, CSS, JavaScript, JSON-Server",
     category: "E-Mail Testing",
     github: "https://github.com/rsoni2843/Mailtrap-Clone",
     description:
@@ -163,8 +164,7 @@ export const projectsData = [
     image: Firstpost,
     name: "Firstpost (Individual)",
     github: "https://github.com/rsoni2843/wrong-mother-4289",
-    stack:
-      "HTML, CSS, JavaScript, React JS, Chakra UI, Bootstrap, React Router DOM.",
+    stack: "React JS, Chakra UI, Bootstrap, React Router DOM",
     category: "News",
     description:
       "Firstpost is an Indian online news and media website. Firstpost is a part of the Network 18 media conglomerate owned by Reliance Industries.",
@@ -173,7 +173,7 @@ export const projectsData = [
   {
     id: "5",
     image: NDTV,
-    stack: "HTML, CSS, JavaScript.",
+    stack: "HTML, CSS, JavaScript",
     name: "NDTV (Collaborative)",
     category: "News",
     github: "https://github.com/rsoni2843/ndtv",
@@ -185,7 +185,7 @@ export const projectsData = [
     id: "10",
     image: Food,
     github: "https://github.com/rsoni2843/Food_App_in_JS_with_Auth",
-    stack: "HTML, CSS, JavaScript, Local Storage.",
+    stack: "HTML, CSS, JavaScript, Local Storage",
     name: "Food App",
     category: "Food Search app",
     description:
@@ -195,7 +195,7 @@ export const projectsData = [
   {
     id: "6",
     image: Youtube,
-    stack: "HTML, CSS, JavaScript, Rest API.",
+    stack: "HTML, CSS, JavaScript, Rest API",
     name: "Youtube",
     github: "https://github.com/rsoni2843/Youtube-Clone-JS",
     category: "Social Media",
@@ -207,30 +207,30 @@ export const projectsData = [
     id: "7",
     image: Weather,
     github: "https://github.com/rsoni2843/Weather_App_in_JS",
-    stack: "HTML, CSS, JavaScript, Rest API.",
+    stack: "HTML, CSS, JavaScript, Rest API",
     name: "Weather App",
     category: "Weather Information",
     description:
       "Weather app provides the weather information across the world for daily and weekly updates.",
     link: "https://splendorous-granita-7d159d.netlify.app/",
   },
-  {
-    id: "8",
-    image: StarWar,
-    github: "https://github.com/rsoni2843/Star-War_character_search_with_JS",
-    stack: "HTML, CSS, JavaScript.",
-    name: "Starwar",
-    category: "Star War Character Search",
-    description:
-      "Star War character search using the debouncing feature in the JavaScript",
-    link: "https://brilliant-unicorn-47c550.netlify.app",
-  },
+  // {
+  //   id: "8",
+  //   image: StarWar,
+  //   github: "https://github.com/rsoni2843/Star-War_character_search_with_JS",
+  //   stack: "HTML, CSS, JavaScript",
+  //   name: "Starwar",
+  //   category: "Star War Character Search",
+  //   description:
+  //     "Star War character search using the debouncing feature in the JavaScript",
+  //   link: "https://brilliant-unicorn-47c550.netlify.app",
+  // },
   {
     id: "9",
     image: Job,
     github:
       "https://github.com/rsoni2843/Job_App_with_JS_with_Sorting-Bookmark",
-    stack: "HTML, CSS, JavaScript, Local Storage.",
+    stack: "HTML, CSS, JavaScript, Local Storage",
     name: "Job App",
     category: "Job app",
     description:
@@ -281,6 +281,9 @@ export const skills = [
     image: SkillImg7,
   },
   {
+    image: "https://assets.stickpng.com/images/5847f40ecef1014c0b5e488a.png",
+  },
+  {
     image: "https://www.coffeeclass.io/logos/chakra-ui.png",
   },
   {
@@ -292,9 +295,6 @@ export const skills = [
   {
     image:
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
-  },
-  {
-    image: "https://assets.stickpng.com/images/5847f40ecef1014c0b5e488a.png",
   },
 ];
 
